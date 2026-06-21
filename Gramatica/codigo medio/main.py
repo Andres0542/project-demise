d9e902bbcf57cf0d399f7f1dd70ca81ab1f8ede3
Anarchy Panty <136258763+Andres0542@users.noreply.github.com>
2026-06-21 17:33:39 -0400

    cambios

--- Gramatica/codigo medio/main.py
+++ Gramatica/codigo medio/main.py
@@ -4,15 +4,6 @@ from gen.DemiseLexer import DemiseLexer
 from gen.DemiseParser import DemiseParser
 from semantic.semantic_visitor import SemanticVisitor
 from codegen.code_gen_visitor import CodeGenVisitor
-from llvmlite import binding
-
-
-def inicializar_llvm():
-    target_triple = binding.get_process_triple()
-    target = binding.Target.from_triple(target_triple)
-    target_machine = target.create_target_machine()
-    data_layout = str(target_machine.target_data)
-    return target_triple, data_layout
 
 
 def main():
@@ -31,17 +22,13 @@ def main():
         return
 
     generador = CodeGenVisitor()
-    try:
-        triple, layout = inicializar_llvm()
-        generador.module.triple = triple or "x86_64-pc-linux-gnu"
-        generador.module.data_layout = layout
-    except Exception as e:
-        print(f"Advertencia: No se pudo auto-detectar el target nativo ({e}). Generando IR genérico.")
-
     generador.visit(tree)
+    
+    # 3. Cierras la función main
     generador.finalizar()
 
-    codigo = generador.emitir("salida.ll")
+    # 4. Emites el código guardándolo en un archivo .c
+    codigo = generador.emitir("salida.c")
     print(codigo)
 
 
---
+++ Gramatica/codigo medio/salida.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <GL/glut.h>
+#include <math.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
+#include "wall.bmp"
+#include "floor.bmp"
+#include "sky.bmp"
+
+int mapW[] = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+static int32_t mapX = 10;
+static int32_t mapY = 6;
+
+int main() {
+    return 0;
+}
